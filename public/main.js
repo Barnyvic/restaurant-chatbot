20202c1d63f1808d14425536d6978a5fe9b1ff5e
Barny Victor <victorbarny4@gmail.com>
2023-03-09 03:21:00 +0100

    added database and model

--- public/main.js
+++ public/main.js
@@ -2,54 +2,24 @@ const chatForm = document.getElementById("chat-form");
 const socket = io("ws://localhost:4000");
 
 socket.on("connected", function (BotWelcomeMsg) {
-  BotWelcomeMessage(BotWelcomeMsg);
+ 
 });
 
 socket.on("Botmessage", (msg) => {
-  outputBot(msg);
 });
+ 
 
-// Message submit
 chatForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const div = document.createElement("div");
 
   const Message = e.target.elements.inputMessage.value;
 
-  outPutMessage(Message);
-
-  //sending Message to the Server
   socket.emit("chatMessage", Message);
 
   e.target.elements.inputMessage.value = "";
 });
 
-//welcome Message of the bot
-const BotWelcomeMessage = (BotWelcomeMsg) => {
-  const divElement = document.getElementById("msg-text");
 
-  divElement.innerHTML = `
-   <p class="h3 display-2"> Welcome to ChatGrill</p>
-   ${BotWelcomeMsg.map(
-     (item) =>
-       `
-        <ul class="list-group"><li class="list-group-item fw-bold h-5">${item.option}</li> </ul>`
-   ).join("")}
-  `;
-};
 
-// OutPut Message to DOM
-const outPutMessage = (Message) => {
-  const div = document.createElement("div");
-  div.classList.add("msg-bubble");
-  div.innerHTML = `<div class="msg-info">
-              <!-- <div class="msg-info-name">Sajad</div> -->
-            </div>
-            <div class="msg-text">${Message}</div>`;
-  document.getElementById("msg-right").appendChild(div);
-};
 
-const outputBot = (msg) => {
-  const divElement = document.getElementById("msg-text");
-  divElement.innerHTML = `<p class="h3 display-2">${msg}</p>`;
-};
--- src/server.js
+++ src/server.js
@@ -1,47 +1,97 @@
-const express = require("express");
-const session = require("express-session");
+const express = require('express');
+const session = require('express-session');
 const app = express();
-const http = require("http");
-const BotWelcomeMessage = require("../data");
+const http = require('http');
+const { dbConnection } = require('./db/dbConfij');
+const orderModel = require('./model/Order');
 
-const PORT = 4000;
+const PORT = 5000;
 const server = http.createServer(app);
 
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 const io = new Server(server);
 
 const sessionMiddleware = session({
-  secret: "tadhaduied82983794",
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 300000 },
+      secret: 'tadhaduied82983794',
+      resave: false,
+      saveUninitialized: true,
+      cookie: { maxAge: 300000 },
 });
 
-app.use(express.static("public"));
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-io.use((socket, next) => {
-  sessionMiddleware(socket.request, socket.request.res, next);
+app.post('/chat', async (req, res) => {
+      const sessionId = req.body.sessionId;
+      const message = req.body.message;
+
+      // TODO: handle session management here
+
+      if (message === '1') {
+            // handle order placement
+            let order = await orderModel.findOne({
+                  sessionId: sessionId,
+                  status: 'in progress',
+            });
+
+            if (!order) {
+                  order = new Order({ sessionId: sessionId });
+            }
+
+            // TODO: retrieve list of menu items from the database and send to the user
+            // once the user selects the items they want to order, add them to the order and save it to the database
+            order.items = ['item 1', 'item 2', 'item 3'];
+            await order.save();
+      } else if (message === '99') {
+            // handle order checkout
+            const order = await orderModel.findOne({
+                  sessionId: sessionId,
+                  status: 'in progress',
+            });
+
+            if (order) {
+                  order.status = 'completed';
+                  await order.save();
+                  res.json({ message: 'Order placed' });
+            } else {
+                  res.json({ message: 'No order to place' });
+            }
+      } else if (message === '98') {
+            // handle order history
+            const orders = await orderModel.find({ sessionId: sessionId });
+            // TODO: format the list of orders and send to the user
+      } else if (message === '97') {
+            // handle current order
+            const order = await Order.findOne({
+                  sessionId: sessionId,
+                  status: 'in progress',
+            });
+
+            if (order) {
+                  // TODO: format the current order and send to the user
+            } else {
+                  res.json({ message: 'No current order' });
+            }
+      } else if (message === '0') {
+            // handle order cancellation
+            const order = await Order.findOne({
+                  sessionId: sessionId,
+                  status: 'in progress',
+            });
+
+            if (order) {
+                  await order.remove();
+                  res.json({ message: 'Order cancelled' });
+            } else {
+                  res.json({ message: 'No order to cancel' });
+            }
+      } else {
+            // handle invalid input
+      }
 });
 
-// run when client connect
-io.on("connection", (socket) => {
-  socket.request.session.socketId = socket.id;
-  socket.request.session.save();
-  console.log("client connected");
-
-  socket.emit("connected", BotWelcomeMessage);
-
-  // Listen for Chat Message
-  socket.on("chatMessage", (message) => {
-    const myArray = ["1", "99", "98", "97", "0"];
-    if (message.includes(myArray)) {
-      console.log("yes");
-    } else {
-      socket.emit("Botmessage", "Wrong");
-    }
-  });
-});
-
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.listen(PORT, async () => {
+      await dbConnection();
+      console.log(`Server listening on ${PORT}`);
 });

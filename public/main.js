e33860ed03c954593bae1f7f98c4b0e0a57480d2
Barny Victor <victorbarny4@gmail.com>
2023-03-12 09:17:33 +0100

    changed some styling

--- public/main.js
+++ public/main.js
@@ -1,35 +1,71 @@
-const chatForm = document.getElementById("chat-form");
-const socket = io("ws://localhost:4000");
+const chatForm = document.getElementById('chat-form');
+const Message = document.getElementById('msg');
+const socket = io('ws://localhost:4000');
 
-socket.on("connected", function (BotWelcomeMsg) {
-  BotWelcomeMessage(BotWelcomeMsg);
+socket.on('bot-message', function (msg) {
+        handleUserMessage(msg);
 });
 
-socket.on("Botmessage", (msg) => {
-  console.log(msg);
+socket.on('chat-message', function (msg) {
+        console.log(msg);
+        handleUserMessage(msg);
 });
+
+const handleUserMessage = (msg) => {
+        //create a div element
+        //create a div element
+        const div = document.createElement('div');
+        div.classList.add('message');
+        //check if the message is from the bot or the user
+        if (msg.sender === 'bot') {
+                div.innerHTML = `<div class="message-right">
+                                        <div>
+                                                <div
+                                                        class="message-content"
+                                                >
+                                                        <p>${msg.message}</p>
+                                                </div>
+                                                <div class="message-meta">
+                                                        <p id="time"></p>
+                                                </div>
+                                        </div>
+                                </div> `;
+        } else {
+                div.innerHTML = `<div class="message-left">
+                                        <div>
+                                                <div
+                                                        class="message-content"
+                                                >
+                                                        <p>${msg}</p>
+                                                </div>
+                                                <div class="message-meta">
+                                                        <p id="time"></p>
+                                                </div>
+                                        </div>
+                                </div>`;
+        }
+        //append the div to the messages div
+        Message.appendChild(div);
+};
 
 // Message submit
-chatForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+chatForm.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-  const Message = e.target.elements.inputMessage.value;
+        let Msg = e.target.elements.inputMessage.value;
 
-  //sending Message to the Server
-  socket.emit("chatMessage", Message);
+        let Message = Msg.trim();
+
+        if (!Message) {
+                return false;
+        }
+
+        if (Message !== '') {
+                console.log(Message);
+                //sending Message to the Server
+                socket.emit('chat-message', Message);
+        }
+        // Clear input
+        e.target.elements.inputMessage.value = '';
+        e.target.elements.inputMessage.focus();
 });
-
-
-//welcome Message of the bot
-const BotWelcomeMessage = (BotWelcomeMsg) => {
-  const divElement = document.getElementById("msg-text");
-
-  divElement.innerHTML = `
-   <p class="h3 display-2"> Welcome to ChatGrill</p>
-   ${BotWelcomeMsg.map(
-     (item) =>
-       `
-        <ul class="list-group"><li class="list-group-item fw-bold h-5">${item.option}</li> </ul>`
-   ).join("")}
-  `;
-};
--- server.js
+++ server.js
@@ -1,44 +1,83 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
-const path = require("path");
+const http = require('http');
+const path = require('path');
+const session = require('express-session');
 
 const PORT = 4000;
 const server = http.createServer(app);
 
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const BotWelcomeMessage = [
-  {
-    option: "Select 1 to Place an order",
-  },
-  {
-    option: "Select 99 to checkout order",
-  },
-  {
-    option: "Select 98 to see order history",
-  },
-  {
-    option: "Select 97 to see current order",
-  },
-  { option: "Select 0 to cancel order" },
-];
+
+const sessionMiddleWare = session({
+        secret: 'tjhbyu278492oyohrdwh8whi',
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+                secure: false,
+                maxAge: 1000 * 60 * 60 * 24 * 7,
+        },
+});
+
+app.use(sessionMiddleWare);
+io.engine.use(sessionMiddleWare);
+
+
+const FoodItems = {
+        2: 'Item1',
+        3: 'Item2',
+        4: 'Item3',
+        5: 'Item4',
+};
 
 // run when client connect
-io.on("connection", (socket) => {
-  console.log("client connected", socket.id);
+io.on('connection', (socket) => {
+        console.log('a user connected', socket.id);
 
-  socket.emit("connected", BotWelcomeMessage);
+        const session = socket.request.session;
+        const sessionId = session.id;
 
-  // Listen for Chat Message
-  socket.on("chatMessage", (message) => {
-    socket.emit("Botmessage", "hello");
-  });
+        socket.join(sessionId);
+
+        io.to(sessionId).emit('bot-message', {
+                sender: 'bot',
+                message: `<b>Welcome to ChatGril...</b> <br> Pls press any of the following keys: <br>
+     <b>select 1 Place Order </b> <br>
+    <b>Select 99 Checkout Order </b> <br>
+   <b>Select 98 Order History</b>  <br>
+   <b>Select 97 See Current Order</b><br>
+    <b>Select 0 Cancel Order</b> <br>`,
+        });
+
+        socket.on('chat-message', (msg) => {
+                io.to(sessionId).emit('chat-message', msg);
+
+                if (msg === '1') {
+                         const items = Object.entries(FoodItems)
+                                 .map(([key, value]) => `${key}. ${value}`)
+                                 .join('\n');
+                                 console.log(items)
+                        io.to(sessionId).emit('bot-message', {
+                                sender: 'bot',
+                                message: `Here is a list of items you can order:<br>${items}<br>Please select one by typing its number.`,
+                        });
+                } else {
+                        io.to(sessionId).emit('bot-message', {
+                                sender: 'bot',
+                                message: `<b>Invalid Input...</b>`,
+                        });
+                }
+        });
+
+        socket.on('disconnect', () => {
+                console.log('user disconnected');
+        });
 });
 
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+        console.log(`Server listening on port ${PORT}`);
 });

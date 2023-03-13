a7f3243a6f1212449623da6bcf42dec36ea70f0a
Barny Victor <victorbarny4@gmail.com>
2023-03-13 10:00:26 +0100

    changes

--- public/main.js
+++ public/main.js
@@ -7,50 +7,13 @@ socket.on('bot-message', function (msg) {
         handleUserMessage(msg);
 });
 
-socket.on('chat-message', function (msg) {
-        handleUserMessage(msg);
+socket.on("Botmessage", (msg) => {
+  console.log(msg);
 });
 
-const handleUserMessage = (msg) => {
-        //create a div element
-        //create a div element
-        const div = document.createElement('div');
-        div.classList.add('message');
-        //check if the message is from the bot or the user
-        if (msg.sender === 'bot') {
-                div.innerHTML = `<div class="message-right">
-                                        <div>
-                                                <div
-                                                        class="message-content"
-                                                >
-                                                        <p>${msg.message}</p>
-                                                </div>
-                                                <div class="message-meta">
-                                                        <p id="time"></p>
-                                                </div>
-                                        </div>
-                                </div> `;
-        } else {
-                div.innerHTML = `<div class="message-left">
-                                        <div>
-                                                <div
-                                                        class="message-content"
-                                                >
-                                                        <p>${msg}</p>
-                                                </div>
-                                                <div class="message-meta">
-                                                        <p id="time"></p>
-                                                </div>
-                                        </div>
-                                </div>`;
-        }
-        //append the div to the messages div
-        Message.appendChild(div);
-};
-
 // Message submit
-chatForm.addEventListener('submit', (e) => {
-        e.preventDefault();
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
         let Msg = e.target.elements.inputMessage.value;
 
@@ -60,12 +23,21 @@ chatForm.addEventListener('submit', (e) => {
                 return false;
         }
 
-        if (Message !== '') {
-                console.log(Message);
-                //sending Message to the Server
-                socket.emit('chat-message', Message);
-        }
-        // Clear input
-        e.target.elements.inputMessage.value = '';
-        e.target.elements.inputMessage.focus();
+  //sending Message to the Server
+  socket.emit("chatMessage", Message);
 });
+
+
+//welcome Message of the bot
+const BotWelcomeMessage = (BotWelcomeMsg) => {
+  const divElement = document.getElementById("msg-text");
+
+  divElement.innerHTML = `
+   <p class="h3 display-2"> Welcome to ChatGrill</p>
+   ${BotWelcomeMsg.map(
+     (item) =>
+       `
+        <ul class="list-group"><li class="list-group-item fw-bold h-5">${item.option}</li> </ul>`
+   ).join("")}
+  `;
+};
